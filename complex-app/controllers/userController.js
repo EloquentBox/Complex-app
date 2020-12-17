74b24265e77842c4965bb940d59af45bf5e830ef
Karol <drogakrzyzowa@gmail.com>
2020-12-17 22:06:23 +0000

    New elements - module and validations

--- complex-app/controllers/userController.js
+++ complex-app/controllers/userController.js
@@ -1,15 +1,23 @@
-exports.login = () => {
+const User = require('../models/User')
+
+exports.login = function() {
 
 }
 
-exports.logout = () => {
+exports.logout = function() {
 
 }
 
-exports.register = (req, res) => {
-    res.send('thanks for trying to register')
+exports.register = function(req, res) {
+    let user = new User(req.body)
+    user.register()
+    if (user.errors.length) {
+        res.send(user.errors)
+    } else {
+        res.send("Congrats, there are no errors.")
+    }
 }
 
-exports.home = (req, res) => {
-    res.render('home-guest');
+exports.home = function(req, res) {
+    res.render('home-guest')
 }
